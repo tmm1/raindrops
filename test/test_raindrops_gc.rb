7a0bb1afb81da3c83f2cc59403826e1f855d3f0d
Eric Wong <normalperson@yhbt.net>
2010-08-03 17:11:32 -0700

    test: make GC test (hopefully) more reliable
    It's not pretty...

--- test/test_raindrops_gc.rb
+++ test/test_raindrops_gc.rb
@@ -4,9 +4,12 @@ require 'raindrops'
 
 class TestRaindropsGc < Test::Unit::TestCase
 
+  # we may need to create more garbage as GC may be less aggressive
+  # about expiring things.  This is completely unrealistic code,
+  # though...
   def test_gc
     assert_nothing_raised do
-      1000000.times { Raindrops.new(24) }
+      1000000.times { |i| Raindrops.new(24); [] }
     end
   end
 
